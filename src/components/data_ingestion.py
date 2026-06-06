c5c674665cca754bea66e9901541344ed05d7207
Swagat35 <swagatpati04@gmail.com>
2026-06-06 12:35:41 +0530

    EDA and problem statement

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str=os.path.join("artifacts/train.csv")
+    test_data_path:str=os.path.join("artifacts/test.csv")                                 
+    raw_data_path:str=os.path.join('artifacts/raw.csv')
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+    def initiate_data_ingestion(self):
+        logging.info("enterd the data ingestion method or component")
+        try:
+            df=pd.read_csv('notebook\data\customer_churn.csv')
+            logging.info('read the dataset')
+        except:
+            pass                                  
